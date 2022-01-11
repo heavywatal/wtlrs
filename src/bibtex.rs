8bbeedf2a3711b7d581504613620d44fc2d1b641
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-01-11 20:22:31 +0900

    :sparkles: Add normalize_pages()

--- src/bibtex.rs
+++ src/bibtex.rs
@@ -1,5 +1,13 @@
 use std::collections::HashSet;
-use regex::Regex;
+use once_cell::sync::OnceCell;
+use regex::{Captures, Regex};
+
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: OnceCell<Regex> = OnceCell::new();
+        RE.get_or_init(|| Regex::new($re).unwrap())
+    }};
+}
 
 pub fn filter(content: &str, citekeys: &HashSet::<String>) -> String {
     let fields = HashSet::from(["author", "title", "journal", "year",
@@ -22,10 +30,30 @@ fn select(entry: &str, fields: &HashSet::<&str>) -> String {
     let mut lines = entry.split_inclusive('\n');
     s.push_str(lines.next().unwrap()); // @article
     for line in lines {
-        if fields.contains(line.split(" = ").next().unwrap().trim_start()) {
-            s.push_str(line);
+        let field = line.split(" = ").next().unwrap().trim_start();
+        if fields.contains(field) {
+            if field == "pages" {
+                s.push_str(&normalize_pages(line));
+            } else {
+                s.push_str(line);
+            }
         }
     }
     s.replace_range((s.len() - 2).., "}\n");
     return s;
+}
+
+fn normalize_pages(line: &str) -> String {
+    let re = regex!(r"^(\s*pages = )\{(\d+)-+(\d+)\}");
+    let repl = |caps: &Captures| -> String {
+        let start = caps[2].to_string();
+        let mut end = caps[3].to_string();
+        if start.len() > end.len() {
+            let mut new_end = start.clone();
+            new_end.replace_range((start.len() - end.len()).., &end);
+            end = new_end;
+        }
+        return format!("{}{{{}--{}}}", &caps[1], start, end);
+    };
+    return re.replace(line, repl).to_string();
 }
